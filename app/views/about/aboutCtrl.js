4823e2d689d578a4bcdf109691674c398457b12a
Carlos Sueldo <thetrip3@live.com>
2016-10-13 16:25:00 -0500

    added controller with repeating photos in about

---
+++ app/views/about/aboutCtrl.js
@@ -0,0 +1,81 @@
+angular.module("myApp")
+.controller("aboutCtrl", function( $scope ){
+
+  $scope.employees = employees;
+});
+
+var employees = [
+  {
+    "picture":"/images/about/employees/1-Ken-1.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/1-Brodie-2.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/1-Kevin-3.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/1-Misbah-4.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/1-Ginger-5.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/1-Yuji-6.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/2-Christopher-1.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/2-Ben-2.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/2-Dixie-3.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/2-Alex-4.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/2-Barrett-5.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/2-Edmund-6.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/3-Sylvia-1.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/3-Danielle-2.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/3-Joe-3.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/3-Galin-4.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/3-Victor-5.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/3-Tommy-6.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/4-Dashia-1.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/4-Jim-2.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/4-Justin-3.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/4-Ashley-4.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/4-Sean-5.jpg" ,
+  } ,
+  {
+    "picture":"/images/about/employees/4-Max-6.jpg" ,
+  }
+
+];
