18891f0efbe39f4b4b4074d626ff3162db80d56d
Carlos Sueldo <thetrip3@live.com>
2016-10-14 04:15:43 -0500

    added products and prices to shopview

--- app/views/about/aboutCtrl.js
+++ app/views/about/aboutCtrl.js
@@ -7,75 +7,98 @@ angular.module("myApp")
 var employees = [
   {
     "picture":"/images/about/employees/1-Ken-1.jpg" ,
+    "hover":"/images/about/employeeshover/1-Kenhover-1.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/1-Brodie-2.jpg" ,
+    "hover":"/images/about/employeeshover/1-Brodiehover-2.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/1-Kevin-3.jpg" ,
+    "hover":"/images/about/employeeshover/1-Kevinhover-3.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/1-Misbah-4.jpg" ,
+    "hover":"/images/about/employeeshover/1-Misbahhover-4.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/1-Ginger-5.jpg" ,
+    "hover":"/images/about/employeeshover/1-Gingerhover-5.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/1-Yuji-6.jpg" ,
+    "hover":"/images/about/employeeshover/1-Yujihover-6.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/2-Christopher-1.jpg" ,
+    "hover":"/images/about/employeeshover/2-Christopherhover-1.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/2-Ben-2.jpg" ,
+    "hover":"/images/about/employeeshover/2-Benhover-2.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/2-Dixie-3.jpg" ,
+    "hover":"/images/about/employeeshover/2-Dixiehover-3.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/2-Alex-4.jpg" ,
+    "hover":"/images/about/employeeshover/2-Alexhover-4.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/2-Barrett-5.jpg" ,
+    "hover":"/images/about/employeeshover/2-Barretthover-5.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/2-Edmund-6.jpg" ,
+    "hover":"/images/about/employeeshover/2-Edmundhover-6.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/3-Sylvia-1.jpg" ,
+    "hover":"/images/about/employeeshover/3-Sylviahover-1.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/3-Danielle-2.jpg" ,
+    "hover":"/images/about/employeeshover/3-Daniellehover-2.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/3-Joe-3.jpg" ,
+    "hover":"/images/about/employeeshover/3-Joehover-3.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/3-Galin-4.jpg" ,
+    "hover":"/images/about/employeeshover/3-Galinhover-4.jpg" ,
   } ,
   {
-    "picture":"/images/about/employees/3-Victor-5.jpg" ,
+    "picture":"/images/about/employees/3-Victor-5.jpg" ,"hover":"/images/about/employeeshover/3-Victorhover-5.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/3-Tommy-6.jpg" ,
+    "hover":"/images/about/employeeshover/3-Tommyhover-6.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/4-Dashia-1.jpg" ,
+    "hover":"/images/about/employeeshover/4-Dashiahover-1.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/4-Jim-2.jpg" ,
+    "hover":"/images/about/employeeshover/4-Jimhover-2.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/4-Justin-3.jpg" ,
+    "hover":"/images/about/employeeshover/4-Justinhover-3.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/4-Ashley-4.jpg" ,
+    "hover":"/images/about/employeeshover/4-Ashleyhover-4.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/4-Sean-5.jpg" ,
+    "hover":"/images/about/employeeshover/4-Seanhover-5.jpg" ,
   } ,
   {
     "picture":"/images/about/employees/4-Max-6.jpg" ,
+    "hover":"/images/about/employeeshover/4-Maxhover-6.jpg" ,
   }
 
 ];
--- app/views/shop/shopCtrl.js
+++ app/views/shop/shopCtrl.js
@@ -5,22 +5,32 @@ angular.module("myApp")
   $scope.lightCases = lightCases;
   $scope.docks = docks;
   $scope.speakers = speakers;
+  $scope.stands = stands;
+  $scope.cups = cups;
 });
 
 var darkCases = [
   {
+    "title": "iPhone Wallet Case" ,
+    "price": "$129" ,
     "case": "/images/shop/cases/1-cases.jpg" ,
     "hover": "/images/shop/caseshover/1-caseshover.jpg" ,
   } ,
   {
+    "title": "Walnut & Leather Case" ,
+    "price": "$109" ,
     "case": "/images/shop/cases/1-cases2.jpg" ,
     "hover": "/images/shop/caseshover/1-caseshover2.jpg" ,
   } ,
   {
+    "title": "Walnut Case" ,
+    "price": "$99" ,
     "case": "/images/shop/cases/1-cases3.jpg" ,
     "hover": "/images/shop/caseshover/1-caseshover3.jpg" ,
   } ,
   {
+    "title": "Walnut Bumper" ,
+    "price": "$59" ,
     "case": "/images/shop/cases/1-cases4.jpg" ,
     "hover": "/images/shop/caseshover/1-caseshover4.jpg" ,
   }
@@ -28,18 +38,26 @@ var darkCases = [
 /*-------------------second row cases ---------*/
 var lightCases = [
   {
+    "title": "iPhone Wallet Case" ,
+    "price": "$129" ,
     "case": "/images/shop/cases/2-cases.jpg" ,
     "hover": "/images/shop/caseshover/2-caseshover.jpg" ,
   } ,
   {
+    "title": "Maple & Leather Case" ,
+    "price": "$109" ,
     "case": "/images/shop/cases/2-cases2.jpg" ,
     "hover": "/images/shop/caseshover/2-caseshover2.jpg" ,
   } ,
   {
+    "title": "Maple Case" ,
+    "price": "$99" ,
     "case": "/images/shop/cases/2-cases3.jpg" ,
     "hover": "/images/shop/caseshover/2-caseshover3.jpg" ,
   } ,
   {
+    "title": "Maple Bumper" ,
+    "price": "$59" ,
     "case": "/images/shop/cases/2-cases4.jpg" ,
     "hover": "/images/shop/caseshover/2-caseshover4.jpg" ,
   }
@@ -47,24 +65,87 @@ var lightCases = [
 /*-------------------Docks---------------------*/
 var docks = [
   {
+    "title": "Walnut Dock" ,
+    "price": "$99" ,
     "dock": "/images/shop/docks/1-docks.jpg" ,
     "hover": "/images/shop/docks/1-dockshover.jpg" ,
   } ,
   {
+    "title": "Maple Dock" ,
+    "price": "$99" ,
     "dock": "/images/shop/docks/1-docks2.jpg" ,
     "hover": "/images/shop/docks/1-dockshover2.jpg" ,
   } ,
   {
+    "title": "Limited Edition Dock" ,
+    "price": "$149" ,
     "dock": "/images/shop/docks/1-docks3.jpg" ,
     "hover": "/images/shop/docks/1-dockshover3.jpg" ,
   }
 ];
-/*-------------------Docks---------------------*/
+/*-------------------Speakers---------------------*/
 var speakers = [
   {
+    "title": "Walnut Speakers & Amp" ,
+    "price": "$599" ,
     "speaker": "/images/shop/speakers/1-speakers.jpg" ,
   } ,
   {
+    "title": "Maple Speakers & Amp" ,
+    "price": "$499" ,
     "speaker": "/images/shop/speakers/1-speakers2.jpg" ,
+  }
+];
+/*-------------------Stands---------------------*/
+var stands = [
+  {
+    "title": "Walnut Monitor Stand" ,
+    "price": "$119" ,
+    "stand": "/images/shop/stands/1-walnutstand.jpg" ,
+    "hover": "/images/shop/stands/1-walnutstand-hover.jpg" ,
+  } ,
+  {
+    "title": "Walnut Macbook Dock" ,
+    "price": "$79" ,
+    "stand": "/images/shop/stands/1-walnutstand2.jpg" ,
+    "hover": "/images/shop/stands/1-walnutstand-hover2.jpg" ,
+  } ,
+  {
+    "title": "Walnut Laptop Stand" ,
+    "price": "$139" ,
+    "stand": "/images/shop/stands/1-walnutstand3.jpg" ,
+    "hover": "/images/shop/stands/1-walnutstand-hover3.jpg" ,
+  }
+];
+/*--------------------WALNUT CUPS ---------------------*/
+var cups = [
+  {
+    "title": "Walnut Pen Cup" ,
+    "price": "$139" ,
+    "cup": "/images/shop/cups/1-cups.jpg" ,
+  } ,
+  {
+    "title": "Walnut Dish" ,
+    "price": "$29" ,
+    "cup": "/images/shop/cups/1-cups2.jpg" ,
+
+  } ,
+  {
+    "title": "Walnut Planter" ,
+    "price": "$29" ,
+    "cup": "/images/shop/cups/1-cups3.jpg" ,
+
+  } ,
+  {
+    "title": "Walnut Planter" ,
+    "price": "$39" ,
+    "cup": "/images/shop/cups/1-cups4.jpg" ,
+
+  } ,
+  {
+    "title": "Walnut Lamp Radio" ,
+    "price": "$99" ,
+    "cup": "/images/shop/cups/1-cups5.jpg" ,
+
   }
 ];
