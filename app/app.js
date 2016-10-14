15f1d1f97cbcfe5b420ab6b9cadb21817787f402
Carlos Sueldo <thetrip3@live.com>
2016-10-14 05:55:18 -0500

    added product views

--- app/app.js
+++ app/app.js
@@ -19,7 +19,95 @@ angular.module( "myApp", [ 'ui.router' ] )
   })
   .state('journal', {
     url: '/journal' ,
-    templateUrl: '../views/journal/journal.html'
-  });
-
+    templateUrl: '../views/journal/journal.html' ,
+  })
+  /*------------product views ---------------*/
+  .state('walnut-wallet', {
+    url:'/walnut-wallet' ,
+    templateUrl: '../views/product/cases/walnut-wallet.html' ,
+  })
+  .state('walnut-leather', {
+    url:'/walnut-leather' ,
+    templateUrl: '../views/product/cases/walnut-leather.html' ,
+  })
+  .state('walnut-case', {
+    url:'/walnut-case' ,
+    templateUrl: '../views/product/cases/walnut-case.html' ,
+  })
+  .state('walnut-bumper', {
+    url:'/walnut-bumper' ,
+    templateUrl: '../views/product/cases/walnut-bumper.html' ,
+  })
+  .state('maple-wallet', {
+    url:'/maple-wallet' ,
+    templateUrl: '../views/product/cases/maple-wallet.html' ,
+  })
+  .state('maple-leather', {
+    url:'/maple-leather' ,
+    templateUrl: '../views/product/cases/maple-leather.html' ,
+  })
+  .state('maple-case', {
+    url:'/maple-case' ,
+    templateUrl: '../views/product/cases/maple-case.html' ,
+  })
+  .state('maple-bumper', {
+    url:'/maple-bumper' ,
+    templateUrl: '../views/product/cases/maple-bumper.html'
+  })
+  /*---------------products views DOCKS ----------*/
+  .state('walnut-dock', {
+    url:'/walnut-dock' ,
+    templateUrl: '../views/product/docks/walnut-dock.html' ,
+  })
+  .state('maple-dock', {
+    url:'/maple-dock' ,
+    templateUrl: '../views/product/docks/maple-dock.html' ,
+  })
+  .state('limited-dock', {
+    url:'/limited-dock' ,
+    templateUrl: '../views/product/docks/limited-dock.html'
+  })
+  /*----------------products views Speakers----------*/
+  .state('walnut-speaker', {
+    url:'/walnut-speaker' ,
+    templateUrl: '../views/product/speakers/walnut-speaker.html' ,
+  })
+  .state('maple-speaker', {
+    url:'/maple-speaker' ,
+    templateUrl: '../views/product/speakers/maple-speaker.html'
+  })
+  /*------------------poducts views Stands --------*/
+  .state('walnut-monitor-stand', {
+    url:'/walnut-monitor-stand' ,
+    templateUrl: '../views/product/stands/walnut-monitor-stand.html' ,
+  })
+  .state('walnut-macbook-dock', {
+    url:'/walnut-macbook-dock' ,
+    templateUrl: '../views/product/stands/walnut-macbook-dock.html' ,
+  })
+  .state('walnut-laptop-stand', {
+    url:'/walnut-laptop-stand' ,
+    templateUrl: '../views/product/stands/walnut-laptop-stand.html'
+  })
+  /*---------------products view Cups ---------*/
+  .state('pen-cup', {
+    url:'/pen-cup' ,
+    templateUrl: '../views/product/cups/pen-cup.html' ,
+  })
+  .state('dish-cup', {
+    url:'/dish-cup' ,
+    templateUrl: '../views/product/cups/dish-cup.html' ,
+  })
+  .state('smallplanter-cup', {
+    url:'/smallplanter-cup' ,
+    templateUrl: '../views/product/cups/smallplanter-cup.html' ,
+  })
+  .state('tallplanter-cup', {
+    url:'/tallplanter-cup' ,
+    templateUrl: '../views/product/cups/tallplanter-cup.html' ,
+  })
+  .state('lamp-cup', {
+    url:'/lamp-cup' ,
+    templateUrl: '../views/product/cups/lamp-cup.html'
+  })
 });
--- app/views/shop/shopCtrl.js
+++ app/views/shop/shopCtrl.js
@@ -15,24 +15,28 @@ var darkCases = [
     "price": "$129" ,
     "case": "/images/shop/cases/1-cases.jpg" ,
     "hover": "/images/shop/caseshover/1-caseshover.jpg" ,
+    "ui":"walnut-wallet" ,
   } ,
   {
     "title": "Walnut & Leather Case" ,
     "price": "$109" ,
     "case": "/images/shop/cases/1-cases2.jpg" ,
     "hover": "/images/shop/caseshover/1-caseshover2.jpg" ,
+    "ui": "walnut-leather" ,
   } ,
   {
     "title": "Walnut Case" ,
     "price": "$99" ,
     "case": "/images/shop/cases/1-cases3.jpg" ,
     "hover": "/images/shop/caseshover/1-caseshover3.jpg" ,
+    "ui": "walnut-case" ,
   } ,
   {
     "title": "Walnut Bumper" ,
     "price": "$59" ,
     "case": "/images/shop/cases/1-cases4.jpg" ,
     "hover": "/images/shop/caseshover/1-caseshover4.jpg" ,
+    "ui": "walnut-bumper" ,
   }
 ];
 /*-------------------second row cases ---------*/
@@ -42,24 +46,28 @@ var lightCases = [
     "price": "$129" ,
     "case": "/images/shop/cases/2-cases.jpg" ,
     "hover": "/images/shop/caseshover/2-caseshover.jpg" ,
+    "ui":"maple-wallet" ,
   } ,
   {
     "title": "Maple & Leather Case" ,
     "price": "$109" ,
     "case": "/images/shop/cases/2-cases2.jpg" ,
     "hover": "/images/shop/caseshover/2-caseshover2.jpg" ,
+    "ui":"maple-leather" ,
   } ,
   {
     "title": "Maple Case" ,
     "price": "$99" ,
     "case": "/images/shop/cases/2-cases3.jpg" ,
     "hover": "/images/shop/caseshover/2-caseshover3.jpg" ,
+    "ui":"maple-case" ,
   } ,
   {
     "title": "Maple Bumper" ,
     "price": "$59" ,
     "case": "/images/shop/cases/2-cases4.jpg" ,
     "hover": "/images/shop/caseshover/2-caseshover4.jpg" ,
+    "ui":"maple-bumper" ,
   }
 ];
 /*-------------------Docks---------------------*/
@@ -69,18 +77,21 @@ var docks = [
     "price": "$99" ,
     "dock": "/images/shop/docks/1-docks.jpg" ,
     "hover": "/images/shop/docks/1-dockshover.jpg" ,
+    "ui":"walnut-dock" ,
   } ,
   {
     "title": "Maple Dock" ,
     "price": "$99" ,
     "dock": "/images/shop/docks/1-docks2.jpg" ,
     "hover": "/images/shop/docks/1-dockshover2.jpg" ,
+    "ui":"maple-dock" ,
   } ,
   {
     "title": "Limited Edition Dock" ,
     "price": "$149" ,
     "dock": "/images/shop/docks/1-docks3.jpg" ,
     "hover": "/images/shop/docks/1-dockshover3.jpg" ,
+    "ui":"limited-dock" ,
   }
 ];
 /*-------------------Speakers---------------------*/
@@ -89,11 +100,13 @@ var speakers = [
     "title": "Walnut Speakers & Amp" ,
     "price": "$599" ,
     "speaker": "/images/shop/speakers/1-speakers.jpg" ,
+    "ui":"walnut-speaker" ,
   } ,
   {
     "title": "Maple Speakers & Amp" ,
     "price": "$499" ,
     "speaker": "/images/shop/speakers/1-speakers2.jpg" ,
+    "ui":"maple-speaker" ,
   }
 ];
 /*-------------------Stands---------------------*/
@@ -103,18 +116,21 @@ var stands = [
     "price": "$119" ,
     "stand": "/images/shop/stands/1-walnutstand.jpg" ,
     "hover": "/images/shop/stands/1-walnutstand-hover.jpg" ,
+    "ui":"walnut-monitor-stand" ,
   } ,
   {
     "title": "Walnut Macbook Dock" ,
     "price": "$79" ,
     "stand": "/images/shop/stands/1-walnutstand2.jpg" ,
     "hover": "/images/shop/stands/1-walnutstand-hover2.jpg" ,
+    "ui":"walnut-macbook-dock" ,
   } ,
   {
     "title": "Walnut Laptop Stand" ,
     "price": "$139" ,
     "stand": "/images/shop/stands/1-walnutstand3.jpg" ,
     "hover": "/images/shop/stands/1-walnutstand-hover3.jpg" ,
+    "ui":"walnut-laptop-stand" ,
   }
 ];
 /*--------------------WALNUT CUPS ---------------------*/
@@ -123,29 +139,30 @@ var cups = [
     "title": "Walnut Pen Cup" ,
     "price": "$139" ,
     "cup": "/images/shop/cups/1-cups.jpg" ,
+    "ui":"pen-cup" ,
   } ,
   {
     "title": "Walnut Dish" ,
     "price": "$29" ,
     "cup": "/images/shop/cups/1-cups2.jpg" ,
-
+    "ui":"dish-cup" ,
   } ,
   {
     "title": "Walnut Planter" ,
     "price": "$29" ,
     "cup": "/images/shop/cups/1-cups3.jpg" ,
-
+    "ui":"smallplanter-cup" ,
   } ,
   {
     "title": "Walnut Planter" ,
     "price": "$39" ,
     "cup": "/images/shop/cups/1-cups4.jpg" ,
-
+    "ui":"tallplanter-cup" ,
   } ,
   {
     "title": "Walnut Lamp Radio" ,
     "price": "$99" ,
     "cup": "/images/shop/cups/1-cups5.jpg" ,
-
+    "ui":"lamp-cup" ,
   }
 ];
