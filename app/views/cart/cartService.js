0f95ba96c153504565cb28c283d2cf88dc52086b
Carlos Sueldo <thetrip3@live.com>
2016-11-17 02:28:28 -0600

    edited cart style

--- app/views/cart/cartService.js
+++ app/views/cart/cartService.js
@@ -5,10 +5,10 @@ angular.module('myApp')
         return $http.post(`${ref.url}/api/cart`);
       },
       addToCart: function(product){
-        return $http.put(`${ref.url}/api/cart/580a53a2de88531a8933c2af`, product)
+        return $http.put(`${ref.url}/api/cart/580a559471b7851ac1ae3352`, product)
       },
       getCart: function(){
-        return $http.get(`${ref.url}/api/cart/580a53a2de88531a8933c2af`)
+        return $http.get(`${ref.url}/api/cart/580a559471b7851ac1ae3352`)
       }
     }
   });
@@ -17,3 +17,4 @@ angular.module('myApp')
 //580a53a2de88531a8933c2af
 //580a556141225d1a99f037cc
 //580a559471b7851ac1ae3352
+//582d433635a38ed9068e051d
